1ca3ed1f8800bcef9742647b5e30d03702ccd062
Amin <mmhdamin.8888@gmail.com>
2024-05-23 23:26:06 +0330

    function insert_course added

--- main.py
+++ main.py
@@ -1,65 +1,132 @@
+import os
 import sqlite3
 import random
+db_path = "Univercity.db"
 
 connection = sqlite3.connect("Univercity.db")
 cursor = connection.cursor()
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Students'")
-result = cursor.fetchone()
-if result == None:
-    cursor.execute("CREATE TABLE Students(id,fwgege,family_name,name,age,student_code)")
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Students'")
+# result = cursor.fetchone()
+# if result == None:
+#     cursor.execute("CREATE TABLE Students(id,family_name,name,age,student_code)")
 
-
-def insert(id,fwgege,family_name,name,age,Student_Code):
-    cursor.execute("INSERT into Students values(?,?,?,?,?,?)",(id,fwgege,family_name,name,age,Student_Code))
+def insert_user(id,family_name,name,age,Student_Code):
+    table_name = "Students"
+    check_table_exists(db_path,table_name)
+    cursor.execute("INSERT into Students values(?,?,?,?,?)",(id,family_name,name,age,Student_Code))
     connection.commit()
     connection.close()
-def delete(Student_Code):
-    cursor.execute("DELETE from Students WHERE Student_Code = ?",(Student_Code,))
+    
+def insert_course(id,math_score,geography_score,Student_code):
+    table_name = "Courses"
+    check_table_exists(db_path,table_name)
+    cursor.execute("INSERT into Courses values(?,?,?,?)",(id,math_score,geography_score,Student_code))
     connection.commit()
     connection.close()
-def edit(Student_Code):
+
+
+def delete_user(Student_Code):
+    table_name = "Students"
+    check_table_exists(db_path,table_name)
+    cursor.execute("DELETE from Students WHERE Student_Code = ?",(Student_Code))
+    connection.commit()
+    connection.close()
+
+def delete_Courses(Student_Code):
+    table_name = "Courses"
+    check_table_exists(db_path,table_name)
+    cursor.execute("DELETE from Courses WHERE Student_Code = ?",(Student_Code))
+    connection.commit()
+    connection.close()
+
+def edit_Students(Student_Code):
     cursor = connection.cursor()
     x = input("What do you want to change? :")
     x = x.lower()
-    if x == "name":
+    if x == "family_name":
+        newfname = input("Please enter your new family_name: ")
+        cursor.execute("UPDATE Students SET family_name = ? WHERE Student_Code = ?",(newfname,Student_Code))
+    elif x == "name":
         newname = input("Please enter your new name :")
         cursor.execute("UPDATE Students SET name = ? WHERE Student_Code = ?",(newname,Student_Code))
     elif x == "age":
         newage = int(input("Please enter your new age :"))
         cursor.execute("UPDATE Students SET age = ? WHERE Student_Code = ?",(newage,Student_Code))
+def edit_Courses(Students_Code):
+    cursor = connection.cursor()
+    x = input("What do you want to change? :")
+    x = x.lower()
+    if x == "math_score":
+        new_math_score = int(input("Please enter your new math score :"))
+        cursor.execute("UPDATE Students SET math_score = ? WHERE Student_Code = ?",(new_math_score,Student_Code))
+    elif x == "geography_score":
+        new_geography_score = int(input("Please enter your new geography score :"))
+        cursor.execute("UPDATE Students SET math_score = ? WHERE Student_Code = ?",(new_geography_score,Student_Code))
+    else:
+        print("ERROR!Please enter a valid data.")
     connection.commit()
     connection.close()
 
-def initialize():
-    connection.execute("SELECT Students from sqlite_master WHERE type='table' AND name=?")
-
-
+def check_table_exists(db_path, table_name):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT name 
+            FROM sqlite_master 
+            WHERE type='table' AND name=?;
+        """, (table_name,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if result:
+            return True
+        else:
+            return False
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return False
+    
 while True:
     operation = input("Please enter a correct operation :")
     operation = operation.lower()
     if operation == "insert":
         id = input("Please enter id :")
-        fwgege = input("Please enter id :")
         family_name = input("Pease enter your family name :")
         name = input("Please enter your name :")
         age = input("Please enter your age :")
         Student_code = random.randint(100_000,999_999)
         print(Student_code)
-        insert(id,fwgege,family_name,name,age,Student_code)
-    elif operation == "delete":
+        insert_user(id,family_name,name,age,Student_code)
+    elif operation == "insert_course":
+        id = input("Please enter id :")
+        math_score = input("Please enter math_score: ")
+        geography_score = input("Please enter geography_score: ")
+        Student_code = int(input("Please enter your Student_Code: "))
+        insert_course(id,math_score,geography_score,Student_code)
+    elif operation == "delete_user":
         Student_Code = input("Please enter your Student Code :")
-        delete(Student_Code)
-    elif operation == "edit":
+        delete_user(Student_Code)
+    elif operation == "delete_Courses":
+        Student_Code = input("Please enter your Student Code :")
+        delete_Courses(Student_Code)
+    elif operation == "edit_Students":
         Student_Code = int(input("Please enter your Student Code :"))
-        edit(Student_Code)
-    elif operation == "show":
+        edit_Students(Student_Code)
+    elif operation == "edit_Courses":
+        Student_Code = int(input("Please enter your Student Code :"))
+        edit_Courses(Student_Code)
+    elif operation == "show_Students":
         cursor.execute("SELECT * from Students")
         row = cursor.fetchall()
         for r in row:
             print(r)
-    elif operation == "initialize":
-        initialize()
+    elif operation == "show_Courses":
+        cursor.execute("SELECT * from Courses")
+        row = cursor.fetchall()
+        for r in row:
+            print(r)
     else:
         print("Please enter a correct operation!")
 
